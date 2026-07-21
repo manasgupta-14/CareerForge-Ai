f8a96dec99b0760fa4d5267c93a2019b1e7605b5
Manas Gupta <manasmggupta@gmail.com>
2026-07-22 01:33:27 +0530

    Modified And Add Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
-    return(
-        <>
-            <h1>Hello</h1>
-        </>
-    )
+import Navbar from "./Components/Navbar/Navbar";
+
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+
+function App() {
+    return (
+        <BrowserRouter>
+
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+
+                <Route path="/jobs" element={<h1>Jobs</h1>} />
+
+                <Route
+                    path="/resume-builder"
+                    element={<h1>AI Resume Builder</h1>}
+                />
+
+                <Route
+                    path="/code-editor"
+                    element={<h1>Online Code Editor</h1>}
+                />
+
+                <Route path="/quiz" element={<h1>Quiz</h1>} />
+
+                <Route path="/about" element={<About />} />
+
+                <Route path="/contact" element={<Contact />} />
+
+                <Route path="/login" element={<h1>Login</h1>} />
+
+                <Route path="/register" element={<h1>Register</h1>} />
+            </Routes>
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <header className="header">
+
+            <div className="container">
+
+                <Link to="/" className="logo">
+                    Career<span>Forge</span> AI
+                </Link>
+
+                <nav className="nav-links">
+                    <Link to="/">Home</Link>
+  
+                    <Link to="/jobs">Jobs</Link>
+
+                    <Link to="/resume-builder">
+                        AI Resume Builder
+                    </Link>
+
+                    <Link to="/code-editor">
+                        Online Code Editor
+                    </Link>
+
+                    <Link to="/quiz">Quiz</Link>
+
+                    <Link to="/about">About</Link>
+
+                    <Link to="/contact">Contact</Link>
+                </nav>
+
+                <div className="auth-buttons">
+                    <Link to="/login" className="login-btn">
+                        Login
+                    </Link>
+
+                    <Link to="/register" className="register-btn">
+                        Register
+                    </Link>
+                </div>
+
+                <button className="menu-btn">
+                    ☰
+                </button>
+
+            </div>
+
+        </header>
+    );
+};
+
+export default Navbar;
