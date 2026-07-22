24a0b20135e505efd47d3b68865ec3cad4b8e607
Manas Gupta <manasmggupta@gmail.com>
2026-07-23 03:04:06 +0530

    Home is Updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,29 @@
+import CareerTips from "../../Components/HomeComponent/CarrerTips/CareerTips";
+import Categories from "../../Components/HomeComponent/Categories/Categories";
+import Companies from "../../Components/HomeComponent/Companies/Companies";
+import FeaturedJobs from "../../Components/HomeComponent/FeaturedJobs/FeaturedJobs";
+import Hero from "../../Components/HomeComponent/Hero/Hero";
+import LatestJobs from "../../Components/HomeComponent/LatestJobs/LatestJobs";
+import Testimonials from "../../Components/HomeComponent/Testimonials/Testimonials";
+import WhyChoose from "../../Components/HomeComponent/WhyChooseUs/WhyChooseUs";
+import NewsLetter from "../../Components/HomeComponent/NewsLetter/NewsLetter";
+import Footer from "../../Components/Footer/Footer";
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+        <>
+            <Hero />
+            <Companies />
+            <Categories />
+            <FeaturedJobs />
+            <LatestJobs />
+            <WhyChoose />
+            <CareerTips />
+            <Testimonials />
+            <NewsLetter />
+            <Footer />
+        </>
+    )
 };
 
 export default Home;
