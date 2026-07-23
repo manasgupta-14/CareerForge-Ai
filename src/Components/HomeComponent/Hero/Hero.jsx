e6ca37d0a2da84b6ef2a3d2430af823c8b58f9fb
Manas Gupta <manasmggupta@gmail.com>
2026-07-23 16:17:40 +0530

    Hero Section Modified

--- src/Components/HomeComponent/Hero/Hero.jsx
+++ src/Components/HomeComponent/Hero/Hero.jsx
@@ -1,78 +1,56 @@
-import "./Hero.css";
+import "./Hero.css"
+import React from 'react'
+import hero from "../../../assets/hero.jpg"
 
 const Hero = () => {
     return (
-        <section className="hero">
+        <>
+            <section className="hero">
+                <div className="hero-container">
+                    <div className="hero-content">
+                        <span className="hero-badge">
+                           🚀 AI Powered Carrer Platform
+                        </span>
 
-            <div className="hero-container">
+                        <h1>
+                            Find <span>Dream Job</span>Faster with CareerForge AI
+                        </h1>
 
-                {/* Left Content */}
+                        <p>
+                            Discover thousands of verified job opportunities from top
+                            companies. Build ATS-friendly resumes, prepare for interviews,
+                            and level up your career with AI.
+                        </p>
 
-                <div className="hero-content">
+                        <div className="hero-search">
+                            <input type="text"
+                                placeholder="Job Title (React Developer)" />
 
-                    <span className="hero-badge">
-                        🚀 AI Powered Career Platform
-                    </span>
+                            <input type="text"
+                                placeholder="Location" />
 
-                    <h1>
-                        Find Your <span>Dream Job</span> Faster with CareerForge AI
-                    </h1>
+                            <button>
+                                Search Jobs
+                            </button>
+                        </div>
 
-                    <p>
-                        Discover thousands of verified job opportunities from top
-                        companies. Build ATS-friendly resumes, prepare for interviews,
-                        and level up your career with AI.
-                    </p>
-
-                    {/* Search */}
-
-                    <div className="hero-search">
-
-                        <input
-                            type="text"
-                            placeholder="Job Title (React Developer)"
-                        />
-
-                        <input
-                            type="text"
-                            placeholder="Location"
-                        />
-
-                        <button>
-                            Search Jobs
-                        </button>
-
+                        <div className="hero-tags">
+                            <span>React</span>
+                            <span>Java</span>
+                            <span>Python</span>
+                            <span>Remote</span>
+                            <span>Internship</span>
+                        </div>
                     </div>
 
-                    {/* Popular Tags */}
-
-                    <div className="hero-tags">
-
-                        <span>React</span>
-                        <span>Java</span>
-                        <span>Python</span>
-                        <span>Remote</span>
-                        <span>Internship</span>
-
+                    <div className="hero-image">
+                        <img src={hero}
+                            alt="Career" />
                     </div>
-
                 </div>
+            </section>
+        </>
+    )
+}
 
-                {/* Right Content */}
-
-                <div className="hero-image">
-
-                    <img
-                        src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=700"
-                        alt="Career"
-                    />
-
-                </div>
-
-            </div>
-
-        </section>
-    );
-};
-
-export default Hero;
+export default Hero
