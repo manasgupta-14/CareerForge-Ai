f6c965263aeaeabe19f72ebb0183267ebfae458c
Manas Gupta <manasmggupta@gmail.com>
2026-07-24 00:35:13 +0530

    Add API of Companies

---
+++ src/API/companiesHome.js
@@ -0,0 +1,68 @@
+import googleLogo from "../assets/google-logo.png"
+import microsoftLogo from "../assets/microsoft-logo.png"
+import amazonLogo from "../assets/amazon-logo.png"
+import netflixLogo from "../assets/netflix-logo.png"
+import adobeLogo from "../assets/adobe-logo.png"
+import infosysLogo from "../assets/infosys-logo.png"
+import tcsLogo from "../assets/tcs-logo.png"
+import wiproLogo from "../assets/wipro-logo.png"
+// import googleLogo from "../assets/google-logo.png"
+
+const companies = [
+    {
+        id:1,
+        logo: googleLogo,
+        name: "Google",
+        jobs: "11 Jobs",
+    },
+     {
+        id:2,
+        logo: microsoftLogo,
+        name: "Microsoft",
+        jobs: "5 Jobs",
+    },
+    {
+        id:3,
+        logo: amazonLogo,
+        name: "Amazon",
+        jobs: "3 Jobs",
+    },
+    {
+        id:4,
+        logo: netflixLogo,
+        name: "Netflix",
+        jobs: "10 Jobs",
+    },
+    {
+        id:5,
+        logo: adobeLogo,
+        name: "Adobe",
+        jobs: "20 Jobs",
+    },
+    {
+        id:6,
+        logo: infosysLogo,
+        name: "Infosys",
+        jobs: "22 Jobs",
+    },
+    {
+        id:7,
+        logo: tcsLogo,
+        name: "TCS",
+        jobs: "30 Jobs",
+    },
+    {
+        id:8,
+        logo: wiproLogo,
+        name: "Wipro",
+        jobs: "7 Jobs",
+    },
+     {
+        id:9,
+        logo: "",
+        name: "Wipro",
+        jobs: "7 Jobs",
+    },
+]
+
+export default companies
