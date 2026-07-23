d49ccd819a7f7976ec6e2dd591f6c3da634bf6ce
Manas Gupta <manasmggupta@gmail.com>
2026-07-24 01:59:39 +0530

    Add API of Categories Home

---
+++ src/API/categoriesHome.js
@@ -0,0 +1,52 @@
+const categories = [
+    {
+        id: 1,
+        icon: "💻",
+        title: "Frontend",
+        jobs: "245 Jobs",
+    },
+    {
+        id: 2,
+        icon: "🖥️",
+        title: "Backend",
+        jobs: "198 Jobs",
+    },
+    {
+        id: 3,
+        icon: "🚀",
+        title: "Full Stack",
+        jobs: "320 Jobs",
+    },
+    {
+        id: 4,
+        icon: "🎨",
+        title: "UI / UX",
+        jobs: "112 Jobs",
+    },
+    {
+        id: 5,
+        icon: "🤖",
+        title: "AI / ML",
+        jobs: "158 Jobs",
+    },
+    {
+        id: 6,
+        icon: "☁️",
+        title: "Cloud",
+        jobs: "135 Jobs",
+    },
+    {
+        id: 7,
+        icon: "📊",
+        title: "Data Analyst",
+        jobs: "174 Jobs",
+    },
+    {
+        id: 8,
+        icon: "🔐",
+        title: "Cyber Security",
+        jobs: "98 Jobs",
+    },
+];
+
+export default categories;
