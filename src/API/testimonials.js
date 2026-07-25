8491ca4b3b059d9bd9fece105c6ea9db04a4b73b
Manas Gupta <manasmggupta@gmail.com>
2026-07-26 01:04:00 +0530

    API is created of Testimonials

---
+++ src/API/testimonials.js
@@ -0,0 +1,49 @@
+const testimonials = [
+    {
+        name: "Rahul Sharma",
+        role: "Frontend Developer",
+        company: "Infosys",
+        image: "https://i.pravatar.cc/150?img=11",
+        review:
+            "CareerForge AI helped me create an ATS-friendly resume. Within two weeks, I got interview calls from multiple companies.",
+        rating: "⭐⭐⭐⭐⭐",
+    },
+    {
+        name: "Priya Verma",
+        role: "Software Engineer",
+        company: "TCS",
+        image: "https://i.pravatar.cc/150?img=32",
+        review:
+            "The AI Resume Builder and Interview Quiz made my preparation much easier. I highly recommend this platform.",
+        rating: "⭐⭐⭐⭐⭐",
+    },
+    {
+        name: "Aman Gupta",
+        role: "React Developer",
+        company: "Wipro",
+        image: "https://i.pravatar.cc/150?img=15",
+        review:
+            "I found my first internship through CareerForge AI. The platform is easy to use and packed with useful features.",
+        rating: "⭐⭐⭐⭐⭐",
+    },
+     {
+        name: "Aman Gupta",
+        role: "React Developer",
+        company: "Wipro",
+        image: "https://i.pravatar.cc/150?img=15",
+        review:
+            "I found my first internship through CareerForge AI. The platform is easy to use and packed with useful features.",
+        rating: "⭐⭐⭐⭐⭐",
+    },
+     {
+        name: "Aman Gupta",
+        role: "React Developer",
+        company: "Wipro",
+        image: "https://i.pravatar.cc/150?img=15",
+        review:
+            "I found my first internship through CareerForge AI. The platform is easy to use and packed with useful features.",
+        rating: "⭐⭐⭐⭐⭐",
+    },
+];
+
+export default testimonials;
