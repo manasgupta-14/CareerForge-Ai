e2480e432ab95e1fa63bda71df9de85bd27d7f34
Manas Gupta <manasmggupta@gmail.com>
2026-07-25 21:57:29 +0530

    API made of Featured JOBS Home

---
+++ src/API/featuredJobsHome.js
@@ -0,0 +1,71 @@
+import googleLogo from "../assets/google-logo.png"
+import microsoftLogo from "../assets/microsoft-logo.png"
+import amazonLogo from "../assets/amazon-logo.png"
+import adobeLogo from "../assets/adobe-logo.png"
+import infosysLogo from "../assets/infosys-logo.png"
+import tcsLogo from "../assets/tcs-logo.png"
+
+const featured_Jobs = [
+    {
+        id:1,
+        company: "Google",
+        logo: googleLogo,
+        title: "Frontend Developer",
+        location: "Noida",
+        salary: "₹8 - 12 LPA",
+        experience: "1-3 Years",
+        type: "Full Time",
+    },
+    {
+        id:2,
+        company: "Microsoft",
+        logo: microsoftLogo,
+        title: "React Developer",
+        location: "Bengaluru",
+        salary: "₹10 - 15 LPA",
+        experience: "2+ Years",
+        type: "Hybrid",
+    },
+    {
+        id:3,
+        company: "Amazon",
+        logo: amazonLogo,
+        title: "Software Engineer",
+        location: "Hyderabad",
+        salary: "₹12 - 18 LPA",
+        experience: "1-4 Years",
+        type: "Full Time",
+    },
+    {
+        id:4,
+        company: "Adobe",
+        logo: adobeLogo,
+        title: "UI/UX Designer",
+        location: "Pune",
+        salary: "₹7 - 11 LPA",
+        experience: "1-2 Years",
+        type: "Remote",
+    },
+    {
+        id:5,
+        company: "Infosys",
+        logo: infosysLogo,
+        title: "Backend Developer",
+        location: "Noida",
+        salary: "₹6 - 10 LPA",
+        experience: "Fresher",
+        type: "Full Time",
+    },
+    {
+        id:6,
+        company: "TCS",
+        logo: tcsLogo,
+        title: "Java Developer",
+        location: "Lucknow",
+        salary: "₹5 - 9 LPA",
+        experience: "Fresher",
+        type: "Hybrid",
+    },
+];
+
+export default featured_Jobs
