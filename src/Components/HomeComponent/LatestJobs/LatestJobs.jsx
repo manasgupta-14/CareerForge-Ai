300342eda1282389b6769e6911ae8fa64d46bdb0
Manas Gupta <manasmggupta@gmail.com>
2026-07-25 22:22:09 +0530

    Modified Latest Jobs

--- src/Components/HomeComponent/LatestJobs/LatestJobs.jsx
+++ src/Components/HomeComponent/LatestJobs/LatestJobs.jsx
@@ -1,49 +1,14 @@
+import { useEffect, useState } from "react";
 import "./LatestJobs.css";
-
-const latestJobs = [
-    {
-        title: "React Developer",
-        company: "Google",
-        location: "Noida",
-        salary: "₹10 - 15 LPA",
-        posted: "2 Hours Ago",
-        type: "Full Time",
-    },
-    {
-        title: "Frontend Developer",
-        company: "Microsoft",
-        location: "Bengaluru",
-        salary: "₹8 - 12 LPA",
-        posted: "5 Hours Ago",
-        type: "Hybrid",
-    },
-    {
-        title: "Backend Developer",
-        company: "Amazon",
-        location: "Hyderabad",
-        salary: "₹12 - 18 LPA",
-        posted: "Today",
-        type: "Remote",
-    },
-    {
-        title: "Java Developer",
-        company: "Infosys",
-        location: "Pune",
-        salary: "₹6 - 9 LPA",
-        posted: "Yesterday",
-        type: "Full Time",
-    },
-    {
-        title: "UI/UX Designer",
-        company: "Adobe",
-        location: "Gurugram",
-        salary: "₹7 - 11 LPA",
-        posted: "1 Day Ago",
-        type: "Remote",
-    },
-];
+import latest_Jobs from "../../../API/latestJobsHome";
 
 const LatestJobs = () => {
+    const [latestJobs, setLatestJobs] = useState([]);
+
+    useEffect(()=>{
+        setLatestJobs(latest_Jobs)
+    })
+    
     return (
         <section className="latest-jobs">
 
