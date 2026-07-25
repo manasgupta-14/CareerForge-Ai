e0d74e1f1c8f0af72f16fddf6e4a262976b7f5f3
Manas Gupta <manasmggupta@gmail.com>
2026-07-25 21:58:30 +0530

    Modified Featured Jobs

--- src/Components/HomeComponent/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/HomeComponent/FeaturedJobs/FeaturedJobs.jsx
@@ -1,83 +1,29 @@
+import { useEffect, useState } from "react";
 import "./FeaturedJobs.css";
+import featured_Jobs from "../../../API/featuredJobsHome";
 
-const jobs = [
-    {
-        company: "Google",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
-        title: "Frontend Developer",
-        location: "Noida",
-        salary: "₹8 - 12 LPA",
-        experience: "1-3 Years",
-        type: "Full Time",
-    },
-    {
-        company: "Microsoft",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
-        title: "React Developer",
-        location: "Bengaluru",
-        salary: "₹10 - 15 LPA",
-        experience: "2+ Years",
-        type: "Hybrid",
-    },
-    {
-        company: "Amazon",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
-        title: "Software Engineer",
-        location: "Hyderabad",
-        salary: "₹12 - 18 LPA",
-        experience: "1-4 Years",
-        type: "Full Time",
-    },
-    {
-        company: "Adobe",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Adobe_Corporate_logo.svg",
-        title: "UI/UX Designer",
-        location: "Pune",
-        salary: "₹7 - 11 LPA",
-        experience: "1-2 Years",
-        type: "Remote",
-    },
-    {
-        company: "Infosys",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Infosys_logo.svg",
-        title: "Backend Developer",
-        location: "Noida",
-        salary: "₹6 - 10 LPA",
-        experience: "Fresher",
-        type: "Full Time",
-    },
-    {
-        company: "TCS",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Tata_Consultancy_Services_Logo.svg",
-        title: "Java Developer",
-        location: "Lucknow",
-        salary: "₹5 - 9 LPA",
-        experience: "Fresher",
-        type: "Hybrid",
-    },
-];
 
 const FeaturedJobs = () => {
+    const [featuredJobs, setFeaturedJobs] = useState([]);
+
+    useEffect(() => {
+        setFeaturedJobs(featuredJobs);
+    }, [])
     return (
         <section className="featured-jobs">
-
             <div className="featured-heading">
-
                 <span>FEATURED JOBS</span>
-
                 <h2>Latest Featured Opportunities</h2>
 
                 <p>
                     Discover hand-picked jobs from top companies and apply instantly.
                 </p>
-
             </div>
-
             <div className="jobs-grid">
 
-                {jobs.map((job, index) => (
+                {featured_Jobs.map((job) => (
 
-                    <div className="job-card" key={index}>
+                    <div className="job-card" key={job.id}>
 
                         <div className="company-info">
 
