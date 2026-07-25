dca6f4f550c3c57b941830db51887aeaea8fdc9d
Manas Gupta <manasmggupta@gmail.com>
2026-07-25 22:22:47 +0530

    Make API of Latest Jobs Home

---
+++ src/API/latestJobsHome.js
@@ -0,0 +1,44 @@
+const latestJobs = [
+    {
+        title: "React Developer",
+        company: "Google",
+        location: "Noida",
+        salary: "₹10 - 15 LPA",
+        posted: "2 Hours Ago",
+        type: "Full Time",
+    },
+    {
+        title: "Frontend Developer",
+        company: "Microsoft",
+        location: "Bengaluru",
+        salary: "₹8 - 12 LPA",
+        posted: "5 Hours Ago",
+        type: "Hybrid",
+    },
+    {
+        title: "Backend Developer",
+        company: "Amazon",
+        location: "Hyderabad",
+        salary: "₹12 - 18 LPA",
+        posted: "Today",
+        type: "Remote",
+    },
+    {
+        title: "Java Developer",
+        company: "Infosys",
+        location: "Pune",
+        salary: "₹6 - 9 LPA",
+        posted: "Yesterday",
+        type: "Full Time",
+    },
+    {
+        title: "UI/UX Designer",
+        company: "Adobe",
+        location: "Gurugram",
+        salary: "₹7 - 11 LPA",
+        posted: "1 Day Ago",
+        type: "Remote",
+    },
+];
+
+export default latestJobs;
