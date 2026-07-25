d51c30a3ee33394e181e22e17456cefa7a888a01
Manas Gupta <manasmggupta@gmail.com>
2026-07-26 01:09:40 +0530

    API is modified of Testimonials

--- src/API/testimonials.js
+++ src/API/testimonials.js
@@ -1,18 +1,19 @@
+import userLogo from "../assets/user-logo.png";
 const testimonials = [
     {
         name: "Rahul Sharma",
         role: "Frontend Developer",
         company: "Infosys",
-        image: "https://i.pravatar.cc/150?img=11",
+        image: userLogo,
         review:
             "CareerForge AI helped me create an ATS-friendly resume. Within two weeks, I got interview calls from multiple companies.",
         rating: "⭐⭐⭐⭐⭐",
     },
     {
-        name: "Priya Verma",
+        name: "Soniya Verma",
         role: "Software Engineer",
         company: "TCS",
-        image: "https://i.pravatar.cc/150?img=32",
+        image: userLogo,
         review:
             "The AI Resume Builder and Interview Quiz made my preparation much easier. I highly recommend this platform.",
         rating: "⭐⭐⭐⭐⭐",
@@ -21,25 +22,25 @@ const testimonials = [
         name: "Aman Gupta",
         role: "React Developer",
         company: "Wipro",
-        image: "https://i.pravatar.cc/150?img=15",
+        image: userLogo,
         review:
             "I found my first internship through CareerForge AI. The platform is easy to use and packed with useful features.",
         rating: "⭐⭐⭐⭐⭐",
     },
      {
-        name: "Aman Gupta",
-        role: "React Developer",
+        name: "Shiva Gupta",
+        role: "React-native Developer",
         company: "Wipro",
-        image: "https://i.pravatar.cc/150?img=15",
+        image: userLogo,
         review:
             "I found my first internship through CareerForge AI. The platform is easy to use and packed with useful features.",
         rating: "⭐⭐⭐⭐⭐",
     },
      {
-        name: "Aman Gupta",
-        role: "React Developer",
+        name: "Manas Gupta",
+        role: "MERN Developer",
         company: "Wipro",
-        image: "https://i.pravatar.cc/150?img=15",
+        image: userLogo,
         review:
             "I found my first internship through CareerForge AI. The platform is easy to use and packed with useful features.",
         rating: "⭐⭐⭐⭐⭐",
