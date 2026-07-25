748d1c96b5c36a9fd287cd17fcb83ebeeb4a73fb
Manas Gupta <manasmggupta@gmail.com>
2026-07-25 23:11:07 +0530

    Career Tips API of Home has Added

---
+++ src/API/carrerTipsHome.js
@@ -0,0 +1,31 @@
+import reactQuestion from "../assets/top-react-question.jpg"
+import howtobuild from "../assets/how-build-res.jpg"
+import interviewpreparation from "../assets/interview_preparation.jpg"
+const tips = [
+    {
+        image: reactQuestion,
+        title: "Top 10 React Interview Questions",
+        description:
+            "Prepare for your React interviews with the most commonly asked questions and answers.",
+        date: "15 July 2026",
+        category: "React",
+    },
+    {
+        image: howtobuild,
+        title: "How to Build an ATS Friendly Resume",
+        description:
+            "Learn how to create an ATS-optimized resume that increases your chances of getting shortlisted.",
+        date: "18 July 2026",
+        category: "Resume",
+    },
+    {
+        image: interviewpreparation,
+        title: "Freshers Interview Preparation Guide",
+        description:
+            "Essential tips to crack your first technical interview with confidence.",
+        date: "20 July 2026",
+        category: "Career",
+    },
+];
+
+export default tips;
